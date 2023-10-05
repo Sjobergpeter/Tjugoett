8c997efbfa0d15a5ff51929db190b7020a6a4e4f
Peter Sjöberg <p-sjoberg@hotmail.com>
2023-10-05 16:48:16 +0200

    !

--- Tjugoett.py
+++ Tjugoett.py
@@ -2,7 +2,7 @@ import random
 import os
 import time
 
-
+# skapar en kortlek och blandar den
 def setDeck():
     ranks = ['Ace', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King']
     suits = ["hearts", "Diamonds", "Clubs", "Spades"]
@@ -16,7 +16,7 @@ def setDeck():
     random.shuffle(deck)
     return deck
 
-
+# Jämför poängen mellan spelaren och datorn och skriver ut vem vinnaren är samt vilka kort dom höll
 def winnerIs():
     os.system("cls" if os.name == "nt" else "clear")
     print((".And the winner is.").center(50))
@@ -61,7 +61,7 @@ def winnerIs():
     print("-"*50)
     input("Press Enter for main menu")
 
-
+# Spelarens / datorns kort i handen
 class hand():
     def __init__(self, name):
         self.hand = []
@@ -69,7 +69,7 @@ class hand():
         self.aceValue = 22
         self.name = name
 
-    
+    # plockar upp det övre kortet i kortleken in i handen
     def PickACard(self, card):
         self.hand.append(card)
         self.value += card['value']
@@ -77,6 +77,7 @@ class hand():
             self.aceValue = self.value + 13
         del deck[0]
     
+    # Efter spelaren / datorn plockar kort körs denna funktion som kollar om man får för mycket i handen och skriver ut nuvarande poäng och kort i handen
     def printHand(self):
         if self.value > 21:
             print(f"{self.name} picked up {self.hand[-1]['rank']} of {self.hand[-1]['suit']} and lost with {self.value}")
@@ -109,7 +110,8 @@ class hand():
             print("-"*40)
 
 
-        
+       # Programmet börjar, skapar en hand för spelare och dator och skapar kortlek. topCard pekar på det första / översta kortet i kortleken.
+       # Låter användaren välja om man ska spela, se regler eller stänga av
 while True:
     player = hand("Player")
     computer = hand("Computer")
@@ -142,6 +144,7 @@ If the computer gets more than 21 points or fewer points than the user, the user
         continue
     
     elif user_input.lower() == ("start"):
+        #
         while True:
             os.system("cls" if os.name == "nt" else "clear")
             print(f"You picked up {topCard['rank']} of {topCard['suit']}")
@@ -172,21 +175,21 @@ If the computer gets more than 21 points or fewer points than the user, the user
             print("Computer's turn!")
             print("-"*40)
             input("Press Enter to continue!")
-        
+
+            # Efter spelaren väljer att inte plocka ett nytt kort så börjar datorn spela här
             while True:
                 os.system("cls" if os.name == "nt" else "clear")
                 computer.PickACard(topCard)
                 print(f"Computer picks a card, it's {topCard['rank']} of {topCard['suit']}")
                 topCard = deck[0]
                 print("-"*40)
-
                 computer.printHand()
-
                 
-
+                # Får datorn över 21 vinner Spelaren och spelet avslutas
                 if computer.value > 21:
                     break
 
+                    # Får datorn värdet 19-21 av Ess (Värde 14) så stannar datorn och sedan annonseras vinnaren
                 elif computer.aceValue >= 19 and computer.aceValue <= 21:
                     print("Computer stops at", computer.aceValue, "on Ace")
                     computer.value = computer.aceValue
@@ -194,6 +197,7 @@ If the computer gets more than 21 points or fewer points than the user, the user
                     winnerIs()
                     break
 
+                    #Får datorn 17 - 21 så stannar datorn, (Då är Ess värt 1, om Ess finns i handen) och sedan annonseras vinnaren
                 elif computer.value >= 17:
                     print ("Computer stops at", computer.value)
                     input("Press Enter to continue!")
